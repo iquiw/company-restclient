414b28d557e9c962fe7c5502e8887c3011b4dfa1
Iku Iwasa <iku.iwasa@gmail.com>
2014-12-23 21:53:11 +0900

    company-restclient initial commit

---
+++ README.rst
@@ -0,0 +1,52 @@
+====================
+ company-restclient
+====================
+
+`Company-mode`_ completion back-end for `restclient-mode`_.
+
+It provides auto-completion for HTTP methods and headers in `restclient-mode`_.
+Completion source is given by `know-your-http-well`_.
+
+
+Installation
+============
+
+Depends
+-------
+* cl-lib
+* `company-mode`_
+* `know-your-http-well`_
+
+
+Setup from Git
+--------------
+1. Install from Git::
+
+     git clone https://github.com/iquiw/company-restclient.git
+
+2. Add ``company-restclient`` to ``company-backends`` after loading `company-mode`_.
+
+   .. code:: emacs-lisp
+
+     (add-to-list 'load-path "/path/to/company-restclient")
+     (add-to-list 'company-backends 'company-restclient)
+
+
+Feature
+=======
+* HTTP method name completion
+
+* HTTP header name completion
+
+  | If header name starts with uppercase character, the completion result is capitalized (e.g. "Content-Type").
+  | Otherwise, the completion result contains lowercase characters only (e.g. "content-type").
+
+* Description about HTTP method and header is shown in minibuffer
+
+License
+=======
+Public domain
+
+.. _company-mode: http://company-mode.github.io/
+.. _restclient-mode: https://github.com/pashky/restclient.el
+.. _know-your-http-well: https://github.com/for-GET/know-your-http-well
---
+++ company-restclient.el
@@ -0,0 +1,93 @@
+;;; company-restclient.el --- company-mode completion back-end for restclient-mode
+
+;; Copyright (C) 2014 by Iku Iwasa
+
+;; Author:    Iku Iwasa <iku.iwasa@gmail.com>
+;; URL:       https://github.com/iquiw/company-restclient
+;; Version:   0.0.0
+;; Package-Requires: ((cl-lib "0.5") (company "0.8.0") (emacs "24") (know-your-http-well "0.2.0"))
+
+;;; Commentary:
+
+;; `company-mode' back-end for `restclient-mode'.
+;;
+;; It provides auto-completion for HTTP methods and headers in `restclient-mode'.
+;; Completion source is given by `know-your-http-well'.
+
+;;; Code:
+
+(require 'company)
+(require 'know-your-http-well)
+
+(defvar company-restclient--current-context nil)
+
+(defun company-restclient--find-context ()
+  "Find context (method, header, body) of the current line."
+  (save-excursion
+    (catch 'result
+      (let ((state 0))
+        (while (and (>= (forward-line -1) 0)
+                    (null (looking-at-p "^#")))
+          (cond
+           ((looking-at-p "^[[:space:]]*$")
+            (cond
+             ((= state 0) (setq state 1))
+             ((= state 2) (setq state 3))))
+           ((= state 0) (setq state 2))
+           ((or (= state 1) (= state 3))
+            (throw 'result 'body))))
+
+        (if (or (= state 0) (= state 1))
+            (throw 'result 'method)
+          (throw 'result 'header))))))
+
+(defun company-restclient-prefix ()
+  "Provide completion prefix at the current point."
+  (cl-case (setq company-restclient--current-context
+                 (company-restclient--find-context))
+    (method (let ((case-fold-search nil)) (company-grab "^[CDGHOPT]*")))
+    (header (company-grab-symbol))))
+
+(defun company-restclient-candidates (prefix)
+  "Provide completion candidates for the given PREFIX."
+  (cl-case company-restclient--current-context
+    (method (all-completions prefix (mapcar #'car http-methods)))
+    (header (all-completions (downcase prefix) (mapcar #'car http-headers)))))
+
+(defun company-restclient-meta (candidate)
+  "Return description of CANDIDATE to display as meta information."
+  (cl-case company-restclient--current-context
+    (method (cl-caadr (assoc candidate http-methods)))
+    (header (cl-caadr (assoc candidate http-headers)))))
+
+(defun company-restclient-post-completion (candidate)
+  "Format CANDIDATE in the buffer according to the current context.
+For HTTP method, insert space after it.
+For HTTP header, capitalize if necessary and insert colon and space after it."
+  (cl-case company-restclient--current-context
+    (method (insert " "))
+    (header (let (start (end (point)))
+              (when (save-excursion
+                      (backward-char (length candidate))
+                      (setq start (point))
+                      (let ((case-fold-search nil))
+                        (looking-at-p "[[:upper:]]")))
+                (delete-region start end)
+                (insert (mapconcat 'capitalize (split-string candidate "-") "-"))))
+            (insert ": "))))
+
+;;;###autoload
+(defun company-restclient (command &optional arg &rest ignored)
+  "`company-mode' completion back-end for `restclient-mode'.
+Provide completion info according to COMMAND and ARG.  IGNORED, not used."
+  (interactive (list 'interactive))
+  (cl-case command
+    (interactive (company-begin-backend 'company-restclient))
+    (prefix (and (derived-mode-p 'restclient-mode) (company-restclient-prefix)))
+    (candidates (company-restclient-candidates arg))
+    (ignore-case 'keep-prefix)
+    (meta (company-restclient-meta arg))
+    (post-completion (company-restclient-post-completion arg))))
+
+(provide 'company-restclient)
+;;; company-restclient.el ends here
